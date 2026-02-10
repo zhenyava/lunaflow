65706f3effdff63a50340de4584c1356208d051d
Evgenii Kozlov <eokozlov@gmail.com>
2026-02-10 23:56:43 +0000

    Dev/fix privacy (#2)
    * add AGENTS.md file
    
    * fix privacy / terms availability pages
    
    * move tailwind to dist offline

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CalendarApp from './components/CalendarApp';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import TermsOfService from './pages/TermsOfService';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<CalendarApp />} />
-        <Route path="/privacy" element={<PrivacyPolicy />} />
-        <Route path="/terms" element={<TermsOfService />} />
-      </Routes>
-    </BrowserRouter>
+    <CalendarApp />
   );
 }
 
